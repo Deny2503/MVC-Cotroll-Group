2ae0724744516920193c580c4e180bb4cd1e28af
Ar1urk0 <arturvasylkivsky@gmail.com>
2025-10-04 19:35:19 +0200

    categoryControler

--- MVC-Cotroll-Group/Controllers/CategoryController.cs
+++ MVC-Cotroll-Group/Controllers/CategoryController.cs
@@ -1,32 +1,114 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using MVC_Cotroll_Group.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
 
 namespace MVC_Cotroll_Group.Controllers
 {
-    public class HomeController : Controller
+    public class CategoryController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
-
         public IActionResult Index()
         {
-            return View();
+            var categories = _context.Categories
+                .Include(c => c.Products)
+                .ToList();
+            return View(categories);
         }
 
-        public IActionResult Privacy()
+
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Category category)
+        {
+            if (id != category.Id)
+            {
+                return BadRequest();
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories
+                .Include(c => c.Products)
+                .FirstOrDefault(c => c.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            if (category.Products.Any())
+            {
+                TempData["Error"] = "Неможливо видалити категорію , бо вона містить продукти.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+                return NotFound();
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Details(int id)
+        {
+            var category = _context.Categories
+                .Include(c => c.Products)
+                .FirstOrDefault(c => c.Id == id);
+            if (category == null)
+                return NotFound();
+            return View(category);
         }
     }
 }
--- MVC-Cotroll-Group/Models/Category.cs
+++ MVC-Cotroll-Group/Models/Category.cs
@@ -1,8 +1,14 @@
-﻿namespace MVC_Cotroll_Group.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace MVC_Cotroll_Group.Models
 {
     public class Category
     {
+
         public int Id { get; set; }
+        [Required(ErrorMessage = "ім`я обов`язкове")]
+        [MaxLength(100, ErrorMessage = "Максимальна довжина 100 символів")]
         public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
     }
 }
