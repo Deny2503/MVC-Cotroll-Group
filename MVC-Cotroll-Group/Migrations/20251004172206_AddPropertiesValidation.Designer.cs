4b9f8e6aa3067a339cdca3fbd2f301786ff77626
NikitaVoliaPC <volanskijnikita2@gmail.com>
2025-10-04 19:22:47 +0200

    Add models validation on propetries

--- MVC-Cotroll-Group/Migrations/20251004172206_AddPropertiesValidation.Designer.cs
+++ MVC-Cotroll-Group/Migrations/20251004172206_AddPropertiesValidation.Designer.cs
@@ -3,6 +3,7 @@ using MVC_Cotroll_Group.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVC_Cotroll_Group.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251004172206_AddPropertiesValidation")]
+    partial class AddPropertiesValidation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MVC-Cotroll-Group/Models/Category.cs
+++ MVC-Cotroll-Group/Models/Category.cs
@@ -1,13 +1,17 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace MVC_Cotroll_Group.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+
+        [StringLength(50)]
+        [Required]
         public string Name { get; set; }
 
-        [JsonIgnore]
         public List<Product> Products { get; set; }
     }
 }
