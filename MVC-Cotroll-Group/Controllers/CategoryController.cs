caf004909358584cd2a78d61f64fdb3242a13186
NikitaVoliaPC <volanskijnikita2@gmail.com>
2025-10-04 19:28:57 +0200

    Fixe naming error in ProductController, CategoryController

--- MVC-Cotroll-Group/Controllers/CategoryController.cs
+++ MVC-Cotroll-Group/Controllers/CategoryController.cs
@@ -4,29 +4,12 @@ using MVC_Cotroll_Group.Models;
 
 namespace MVC_Cotroll_Group.Controllers
 {
-    public class HomeController : Controller
+    public class CategoryController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
