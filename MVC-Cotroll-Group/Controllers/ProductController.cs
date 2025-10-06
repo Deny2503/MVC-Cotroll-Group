db767b414a4ba6e7e66a31ab87eced0ca82e6fb0
Semen Domin <deminsem2008@gmail.com>
2025-10-06 17:59:14 +0200

    ProductController

--- MVC-Cotroll-Group/Controllers/ProductController.cs
+++ MVC-Cotroll-Group/Controllers/ProductController.cs
@@ -1,67 +1,111 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MVC_Cotroll_Group.Models;
 using MVC_Cotroll_Group.Repositories;
-
 
 namespace MVC_Cotroll_Group.Controllers
 {
     public class ProductController : Controller
     {
-
         private readonly ProductRepository _productRepository;
+        private readonly CategoryRepository _categoryRepository;
 
-        public ProductController(ProductRepository productRepository)
+        public ProductController(ProductRepository productRepository, CategoryRepository categoryRepository)
         {
             _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var products = await _productRepository.GetAllWithCategoryAsync();
+            return View(products);
         }
+
         [HttpGet]
-        public IActionResult Create()
+        public async Task<IActionResult> Details(int id)
         {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null) return NotFound();
+
+            return View(product);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Product product)
+        public async Task<IActionResult> Create(Product product)
         {
+            if (!ModelState.IsValid)
+            {
+                var categories = await _categoryRepository.GetAllAsync();
+                ViewBag.Categories = new SelectList(categories, "Id", "Name", product.CategoryId);
+                return View(product);
+            }
+
+            await _productRepository.AddAsync(product);
+            await _productRepository.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         [HttpGet]
-        public IActionResult Edit(int id)
+        public async Task<IActionResult> Edit(int id)
         {
-            return View();
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null) return NotFound();
+
+            var categories = await _categoryRepository.GetAllAsync();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name", product.CategoryId);
+            return View(product);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, Product product)
+        public async Task<IActionResult> Edit(int id, Product product)
         {
+            if (id != product.Id) return BadRequest();
+
+            if (!ModelState.IsValid)
+            {
+                var categories = await _categoryRepository.GetAllAsync();
+                ViewBag.Categories = new SelectList(categories, "Id", "Name", product.CategoryId);
+                return View(product);
+            }
+
+            await _productRepository.UpdateAsync(product);
+            await _productRepository.SaveChangesAsync();
+
             return RedirectToAction(nameof(Index));
         }
 
         [HttpGet]
-        public IActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
-            return View();
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null) return NotFound();
+
+            return View(product);
         }
 
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            return RedirectToAction(nameof(Index));
-        }
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product != null)
+            {
+                await _productRepository.DeleteAsync(id);
+                await _productRepository.SaveChangesAsync();
+            }
 
-        [HttpGet]
-        public IActionResult Details(int id)
-        {
-            return View();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
