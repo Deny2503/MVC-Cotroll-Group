1553d3d43f19ecac77fac51edf7332e0fb9eb155
Semen Domin <deminsem2008@gmail.com>
2025-10-06 17:10:55 +0200

    View: Index, Create, Edit, Delete, Details

--- MVC-Cotroll-Group/Controllers/ProductController.cs
+++ MVC-Cotroll-Group/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MVC_Cotroll_Group.Models;
+using MVC_Cotroll_Group.Repositories;
 
 
 namespace MVC_Cotroll_Group.Controllers
@@ -7,7 +8,58 @@ namespace MVC_Cotroll_Group.Controllers
     public class ProductController : Controller
     {
 
+        private readonly ProductRepository _productRepository;
+
+        public ProductController(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Product product)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
         {
             return View();
         }
